bb283dbcbbb02f6ca14fdab3eef007e1d8a9a53c
Gauri <potdargau.14@gmail.com>
2026-07-17 14:31:44 +0530

    chore: initialize React + Typescript + Tailwind CSS Setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-rose-900">
+      <h1 className="text-5xl font-bold text-yellow-400">
+        React + Tailwind + TypeScript 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
